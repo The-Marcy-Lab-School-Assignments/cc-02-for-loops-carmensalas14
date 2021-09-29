c587bdaaa8acceb6ac76e5f93384fde659d55c95
carmen <carmensalas0114@gmail.com>
2021-09-29 14:49:30 +0000

    finished problems 1-5

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
+/*
+for ([initial Expression]; [condition Expression]; [increment Expression]){
+
+[statement]
+*/
 //1
 function countToTen(){
-  
+    for(let i=1; i<=10;i+=1){
+        console.log(i)
+    }
 }
-
+// countToTen()
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+    for(let i =1; i <= number; i++){
+        console.log(i)
+    }
 }
-
+// countFromOne(5)
 //3
-function countEveryEven(){
-  
+/*
+== equailty operator 
+=== strict equality operator
+'4' == 4 //true
+'4' === 4 //false
+*/
+// console.log('4' == 4 )
+// console.log('4' === 4 )
+function countEveryEven(number){
+  for(let i =1; i <= number; i++){
+      if(i%2 === 0){
+          console.log(i)
+      }
+    }
 }
-
+// countEveryEven(10)
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(int){
+    for(let i = 1; i<= int; i++){
+        if(i % 2 !== 0){
+            console.log(i)
+        }
+    }
 }
-
+// countEveryOdd(10)
 //5 
-function countEvens(){
-  
+
+function countEvens(numbers){
+    let count = 0
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 ===0){
+            count+=1
+        }
+    }
+  return count
 }
 
+console.log(countEvens([1,2,3,4,5,6,7,8,9,12]))
